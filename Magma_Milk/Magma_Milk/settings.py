c4da66c6bea32d1b18282d8129f1acd83871cb55
Akashrasheeedanjum <87830594+Akashrasheeedanjum@users.noreply.github.com>
2023-09-10 00:56:38 -0700

    Add files via upload

--- Magma_Milk/Magma_Milk/settings.py
+++ Magma_Milk/Magma_Milk/settings.py
@@ -117,7 +117,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+
+
+
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Other settings...
+
+
+
+# Define the STATIC_ROOT setting
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- Magma_Milk/Magma_Milk/urls.py
+++ Magma_Milk/Magma_Milk/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('Store.urls'))
+    path("",include('Store.urls')),
+
+
+
+    # url(r'^media/(?P<path>.*)$',server,{'document_root': settings.MEDIA_ROOT}),
+    # url(r'^staticfile/(?P<path>.*)$',server,{'document_root': settings.STATIC_ROOT}),
+
+
+
+
 ]
+# Add the following line to serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
